b34f53b0d221dddf70d802ac88f2a0c375bf8650
amjadmehmoodnoborderz <amjad@noborderz.com>
2024-07-10 18:07:17 +0500

    backend deleted

--- src/services/sendRequest.ts
+++ src/services/sendRequest.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:8080/api/v1'
+const BASE_URL = 'https://portfolio-mu-taupe-40.vercel.app/api/v1'
 
 
 export const sendRequest = async (payload:any) => {

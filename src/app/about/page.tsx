a78fdaa3f269b3de344520b30806b51f7f6def80
Amjad <amjadmalikf53@gmail.com>
2025-10-29 21:22:47 +0500

    version conflicts resolved

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import AboutMe from '@/components/AboutMe/AboutMe'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-[150px]'>
+        <AboutMe/>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/services/page.tsx
@@ -0,0 +1,12 @@
+import MyServices from '@/components/MyServices/MyServices'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='mt-[50px]'>
+        <MyServices/>
+    </div>
+  )
+}
+
+export default page
